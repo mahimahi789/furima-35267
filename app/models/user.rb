1ec964a823bc59af6a4fcdd7e61506b3dd80d463
mahimahi 789 <daiti0811@icloud.com>
2021-04-26 22:16:08 +0900

    テストコード６

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :j_name, format: {with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "には全角文字を使用してください"}
-    validates :j_k_name, format: {with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "全角漢字ひらがなカタカナを使用してください"}
-    validates :j_k_n_name, format: {with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナを使用してください"}
-    validates :j_k_n_c_name, format: {with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナを使用してください"}
+    validates :j_k_name, format: {with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "には全角文字を使用してください"}
+    validates :j_k_n_name, format: {with: /\A[ァ-ヶー－]+\z/, message: "には全角カタカナを使用してください"}
+    validates :j_k_n_c_name, format: {with: /\A[ァ-ヶー－]+\z/, message: "には全角カタカナを使用してください"}
     validates :password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
     validates :birthday
   end   
