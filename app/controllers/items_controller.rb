30b35279d124b2df932c1d3499a5a8dc8c291c6b
mahimahi 789 <daiti0811@icloud.com>
2021-05-02 10:16:55 +0900

    商品情報編集機能3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!,only: [:new, :create]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :update, :edit]
 
   
 
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
+    #@item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
@@ -47,8 +47,5 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name,:item_description,:category_id,:condition_id,:burden_id,:area_id,:days_id,:price,:image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    redirect_to new_user_session_path unless user_signed_in?
-  end
   
 end
